a4db6773e0c901722e4a2325fa0fdc9d8a70bf0e
Juan David Castañeda Betancourt <jdcb30112005@gmail.com>
2022-09-11 21:14:54 -0500

    Uploading somethings

--- Resources/script.js
+++ Resources/script.js
@@ -15,9 +15,24 @@ gsap.to('#xd',{
     
     display: 'none'
 });
+gsap.to('#load, #loader',{
+    delay: 3,
+    duration: 2,
+    color: 'transparent',
+    backgroundColor: 'transparent',
+    display: 'none'
+});
+
 gsap.from('nav',{
     delay: 5.5,
     y: -150,
     duration: .7,
+    ease: 'lineal'
+})
+let loader = gsap.timeline({ repeat: 8, yoyo: true});
+
+loader.to('#load',{
+    duration: .5,
+    x: 145,
     ease: 'lineal'
 })
